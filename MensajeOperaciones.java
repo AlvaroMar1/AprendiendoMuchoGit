343c20fcc78e74a64391df66ab73f50c537a32e3
Álvaro Marqués <alvmarmor6@alu.edu.gva.es>
2026-01-15 12:25:23 +0100

    Creado mensajeoperaciones.java

---
+++ MensajeOperaciones.java
@@ -0,0 +1,9 @@
+public class MensajeOperaciones {
+  public void mostrarMensajeInicio() {
+    System.out.println("Bienvenido al programa de operaciones");
+  }
+  public void mostrarMensajeError() {
+    System.out.println("Gracias por usar el programa de operaciones");
+  }
+}
+    
